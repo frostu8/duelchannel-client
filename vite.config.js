0155b0a2fe4b7228a0ba832a68cea22c753509d4
Dante Helmore <frostu8@protonmail.com>
2026-05-25 19:09:16 -0700

    setup reverse proxy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		proxy: {
+			'/api/v1': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1/, ''),
+      },
+		},
+	},
 	plugins: [sveltekit()]
 });
