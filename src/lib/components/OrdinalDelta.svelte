a89acdb86554cedf7076c391a6ab123c59cf2d83
Dante Helmore <frostu8@protonmail.com>
2026-08-31 18:23:05 -0700

    add padding to rating delta

--- src/lib/components/OrdinalDelta.svelte
+++ src/lib/components/OrdinalDelta.svelte
@@ -22,7 +22,10 @@
 			return `±${scoped}`;
 		}
 	};
-	const ordinalDeltaSub = () => Math.abs(Math.floor((delta - Math.trunc(delta)) * 100)).toFixed(0);
+	const ordinalDeltaSub = () =>
+		Math.abs(Math.floor((delta - Math.trunc(delta)) * 100))
+			.toFixed(0)
+			.padStart(2, '0');
 </script>
 
 <span class="ordinal">
